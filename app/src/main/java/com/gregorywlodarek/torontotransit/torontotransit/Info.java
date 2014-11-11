7ddd8cd214165825ace29a885350368801a04fba
Turgus <gregorywlodarek@gmail.com>
2014-11-11 05:06:48 -0500

    License

--- app/src/main/java/com/gregorywlodarek/torontotransit/torontotransit/Info.java
+++ app/src/main/java/com/gregorywlodarek/torontotransit/torontotransit/Info.java
@@ -1,5 +1,25 @@
+// Copyright 2014 Grzegorz Wlodarek
+// Distributed under the terms of the GNU General Public License.
+//
+// This file is part of Assignment 1, CSC148, Winter 2014.
+//
+// This is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This file is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this file.  If not, see <http://www.gnu.org/licenses/>.
+
+//Package
 package com.gregorywlodarek.torontotransit.torontotransit;
 
+//Imports
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
